adcaf8a179da04a23b4322d2f8f07bcb8986c627
GreenBNN <kgh980316@gmail.com>
2022-11-16 21:22:37 +0900

    error fix

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -8,7 +8,6 @@ import axios from 'axios'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faGear, faToggleOn, faToggleOff } from '@fortawesome/free-solid-svg-icons'
-import { create } from 'core-js/core/object'
 library.add(faGear, faToggleOn, faToggleOff)
 
 const app = createApp(App)
